6f3c91804382c89e8cc2aa1efa8250377911d0f4
lucilajuliopizarro <lucitajulio@hotmail.com>
2019-08-03 17:54:56 -0400

    creacion de componentes,servicios,navbar,componentes compartidos,componentes comunes,instalacion de boostrap

--- desafio/src/app/app.module.ts
+++ desafio/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServicesComponent } from './components/services/services.component';
-import { SharedComponent } from './components/shared/shared.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServicesComponent,
-    SharedComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
